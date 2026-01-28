0c30748a4edcfffcecbfca28d48d266d7c5f8dbe
Kishin Suematsu <suematsu0987@gmail.com>
2026-01-27 16:44:22 -0800

    Initial Commit

---
+++ MyKakeibo/MyKakeiboApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyKakeiboApp.swift
+//  MyKakeibo
+//
+//  Created by Kishin Suematsu on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct MyKakeiboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
